03ebe6d6a479b0d9d6e01bd039881e934efdfae1
letusfly85 <letusfly95@gmail.com>
2017-12-05 10:39:02 +0900

    start impl signIn example with PlayFramework

---
+++ src/components/Authenticate.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios'
+axios.defaults.xsrfHeaderName = 'Csrf-Token'
+axios.defaults.xsrfCookieName = 'PLAY_CSRF_TOKEN'
+
+const baseUrl = 'http://localhost:9000'
+
+const config = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
+
+export default {
+  authenticate: function (path, params) {
+    let targetPath = baseUrl + path
+    return axios.post(targetPath, params, config)
+    .then((res) => {
+      console.log(res.data)
+      this.$emit('SIGN_IN_SUCCESEED')
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }
+}
+</script>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="sign in">
+    <h1>{{ msg }}</h1>
+    <form>
+      <p>email: <input type="text"></p>
+      <p>password: <input type="text"></p>
+      <button v-on:click="signIn()">Sign In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import Auth from './Authenticate.vue'
+export default {
+  name: 'SignIn',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    signIn: function (event) {
+      console.log('sign in')
+      Auth.authenticate('/signIn', {email: 'test@gmail.com', password: 'test', rememberMe: true})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
