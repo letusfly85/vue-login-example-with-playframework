9d72815406adedeba10c1635fde08714288a10e2
letusfly85 <letusfly95@gmail.com>
2018-01-04 12:33:57 +0900

    modify app

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-right:  5px;
+  margin-bottom: 5px;
 }
 </style>
